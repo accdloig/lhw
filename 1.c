8fe6fe0cba9889db88ddebf6888f32c28668bbb4
accdloig <3181252863@qq.com>
2025-10-25 15:39:54 +0800

    Add temperature conversion from Celsius to Fahrenheit
    This program converts a temperature from Celsius to Fahrenheit.

---
+++ 1.c
@@ -0,0 +1,14 @@
+//202510115125
+//3181252863@qq.com
+//阮海涛
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+    float a = 0;
+   
+    scanf("%f",&a);
+    
+    float c =( a * 9/5)+32;
+    printf("%.1f",c);
+    return 0;
