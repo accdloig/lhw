bfb3810f1e94db291f2cb40f4517e5de76e257b1
accdloig <3181252863@qq.com>
2025-10-25 15:43:58 +0800

    Add triangle validation program in 4.c

---
+++ 4.c
@@ -0,0 +1,21 @@
+//202510115125
+//3181252863@qq.com
+//阮海涛
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    // 输入三个整数
+    scanf("%d %d %d", &a, &b, &c);
+
+    // 判断任意两边之和是否大于第三边
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        printf("可以组成三角形\n");
+    }
+    else {
+        printf("不能组成三角形\n");
+    }
+
+    return 0;
+}
