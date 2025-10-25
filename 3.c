b8f6c185c6dee00df29555d3b4766a85805b48e0
accdloig <3181252863@qq.com>
2025-10-25 15:43:20 +0800

    Add program to sum two integers

---
+++ 3.c
@@ -0,0 +1,14 @@
+//202510115125
+//3181252863@qq.com
+//阮海涛
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf(" %d %d",&num1,&num2);
+	int sum = num1 + num2;
+	printf("%d\n", sum);
+	return  0;
+}
